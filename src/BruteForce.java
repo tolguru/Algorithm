04478524ea8216c6cb7410d122dafeda74052be8
tolguru <rnjsehdgks01@naver.com>
2025-05-21 12:03:53 +0900

    push 테스트

---
+++ src/BruteForce.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BruteForce {
+    public static void main(String[] args) {
+        bj_2309();
+    }
+
+    public static void bj_2309() {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
