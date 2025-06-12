b1a7e26670f988d5eda2fb34a6cd9037ef4e3e96
tolguru <rnjsehdgks01@naver.com>
2025-06-12 18:09:05 +0900

    1629 진행

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Test {
-    public static void main(String[] args) {
-        int result = (9 / 4) * (9 / 4) * (6 / 4);
-        System.out.println(result);
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+//        System.out.println(Math.log(2));
+//        int result = (Integer.parseInt(st.nextToken()) >> 0) * (Integer.parseInt(st.nextToken()) >> 0) * (Integer.parseInt(st.nextToken()) >> 0);
+//        int result = (Integer.parseInt(st.nextToken()) >> 0) * (Integer.parseInt(st.nextToken()) >> 0) * (Integer.parseInt(st.nextToken()) >> 0);
+//        System.out.println(result);
+
+        System.out.println(Math.pow(10, 11) % 12);
     }
 }
---
+++ src/divide_and_conquer/Bj_1629.java
@@ -0,0 +1,16 @@
+package divide_and_conquer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Bj_1629 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        System.out.println(Math.pow(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())) % Integer.parseInt(st.nextToken()));
+    }
+}
